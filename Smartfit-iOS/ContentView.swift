e7892d57556f6e838b10fde43cafa8e72c570a97
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-04-23 18:30:50 +0530

    Initial Commit

---
+++ Smartfit-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smartfit-iOS
+//
+//  Created by ITEDP on 2025-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smartfit-iOS/Smartfit_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smartfit_iOSApp.swift
+//  Smartfit-iOS
+//
+//  Created by ITEDP on 2025-04-23.
+//
+
+import SwiftUI
+
+@main
+struct Smartfit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
